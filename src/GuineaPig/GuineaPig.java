7bf5cd23d531936b8eee8093a2c6b72b722b5651
AlexArnold01 <alar1000@stud.ek.dk>
2025-09-29 15:14:50 +0200

    initial commit

--- src/GuineaPig/GuineaPig.java
+++ src/GuineaPig/GuineaPig.java
@@ -1,5 +1,16 @@
 package GuineaPig;
 
 public class GuineaPig {
+    String name;
+    String race;
+    int feed;
+
+    //constructor
+    public GuineaPig(String name, String race, int feed) {
+        this.name = name;
+        this.race = race;
+        this.feed = feed;
+    }
+
 
 }
--- src/GuineaPig/GuineaPigTest.java
+++ src/GuineaPig/GuineaPigTest.java
@@ -1,4 +1,15 @@
 package GuineaPig;
 
+import java.util.ArrayList;
+
 public class GuineaPigTest {
+
+    public static void main(String[] args) {
+
+    }
+
+    public void run() {
+        ArrayList<GuineaPig> guineaPigs = new ArrayList<>();
+
+    }
 }
