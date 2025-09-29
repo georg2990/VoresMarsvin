b14410bb72754f719ce628cb5991a79c4f8411dc
georg2990 <georg2990@gmail.com>
2025-09-29 15:27:26 +0200

    Initial commit

--- src/GuineaPig/GuineaPigTest.java
+++ src/GuineaPig/GuineaPigTest.java
@@ -5,6 +5,10 @@ import java.util.ArrayList;
 public class GuineaPigTest {
 
     public static void main(String[] args) {
+        String filename = "marsvin.txt";
+
+        int count = 0;
+        try (BufferedReader)
 
     }
 
